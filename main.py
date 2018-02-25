5254f68e721ea436867ccfb9fc67fc492ced186b
Ioffe, Robert <robert.ioffe@intel.com>
2018-02-25 08:40:23 -0800

    untested - image darkening

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import helper
 import warnings
 from distutils.version import LooseVersion
 import project_tests as tests
+import cv2
 # Import everything needed to edit/save/watch video clips
 from moviepy.editor import VideoFileClip
 from IPython.display import HTML
@@ -163,6 +164,59 @@ def pipeline_helper(img, sess, logits, keep_prob, input_image):
 def pipeline(sess, logits, keep_prob, input_image):
     g = lambda img: pipeline_helper(img, sess, logits, keep_prob, input_image)
     return g
+
+# Given the input image,  randomly darken the image. Return darkened image.
+# Image is darkened by reducing the V channel of an HSV image. Note that the range to darken was found empirically 
+def random_dark_image(im):
+    h_, s, v = cv2.split(im)
+
+    darken = randint(0, 125)
+    v = np.where(v > darken, v - darken, 0)
+
+    im = cv2.merge((h_, s, v))
+    return im
+
+def my_gen_batch_function(data_folder, image_shape):
+    """
+    Generate function to create batches of training data
+    :param data_folder: Path to folder that contains all the datasets
+    :param image_shape: Tuple - Shape of image
+    :return:
+    """
+    def get_batches_fn(batch_size):
+        """
+        Create batches of training data
+        :param batch_size: Batch Size
+        :return: Batches of training data
+        """
+        image_paths = glob(os.path.join(data_folder, 'image_2', '*.png'))
+        label_paths = {
+            re.sub(r'_(lane|road)_', '_', os.path.basename(path)): path
+            for path in glob(os.path.join(data_folder, 'gt_image_2', '*_road_*.png'))}
+        background_color = np.array([255, 0, 0])
+
+        random.shuffle(image_paths)
+        for batch_i in range(0, len(image_paths), batch_size):
+            images = []
+            gt_images = []
+            for image_file in image_paths[batch_i:batch_i+batch_size]:
+                gt_image_file = label_paths[os.path.basename(image_file)]
+
+                image = scipy.misc.imresize(scipy.misc.imread(image_file), image_shape)
+                gt_image = scipy.misc.imresize(scipy.misc.imread(gt_image_file), image_shape)
+
+                gt_bg = np.all(gt_image == background_color, axis=2)
+                gt_bg = gt_bg.reshape(*gt_bg.shape, 1)
+                gt_image = np.concatenate((gt_bg, np.invert(gt_bg)), axis=2)
+
+                image = random_dark_image(image) 
+
+                images.append(image)
+                gt_images.append(gt_image)
+
+            yield np.array(images), np.array(gt_images)
+    return get_batches_fn
+
 
 def run():
     num_classes = 2
